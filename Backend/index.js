ac9ba4e262b27b5edc046ef3dc41d1ebd0b0a389
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-08-10 04:23:09 +0530

    chore: Update CORS origin to use production URL for backend server

--- Backend/index.js
+++ Backend/index.js
@@ -26,28 +26,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(
-//   cors({
-//     origin: "https://event-management-two-iota.vercel.app",
-//     credentials: true,
-//   })
-// );
-const allowedOrigins = [
-  "https://event-management-two-iota.vercel.app", // Add your Vercel frontend URL here
-  "http://localhost:5173", // Optional: Allow local development as well
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://event-management-two-iota.vercel.app",
     credentials: true,
   })
 );
