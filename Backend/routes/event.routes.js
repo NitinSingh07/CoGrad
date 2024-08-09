06754b29e3c97d7784192f15824ff9bb52d29e61
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-08-09 22:28:58 +0530

    chore: Update event routes to match API changes

--- Backend/routes/event.routes.js
+++ Backend/routes/event.routes.js
@@ -1,6 +1,4 @@
 import express from "express";
-
-const router = express.Router();
 import {
   createEvent,
   getAllEvents,
@@ -10,15 +8,16 @@ import {
   registerForEvent,
   getRegisteredEvents,
 } from "../controllers/event.controller.js";
-
 import auth from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
 
 router.post("/create-event", auth, createEvent);
 router.get("/getAll", getAllEvents);
 router.get("/getevent/:id", getEvent);
-router.put("/update-event/:id", auth, updateEvent);
-router.delete("/delete-event/:id", auth, deleteEvent);
-router.post("/event/:id/register", auth, registerForEvent);
+router.put("/update-event/:id", updateEvent);
+router.delete("/delete-event/:id", deleteEvent);
+router.post("/event/register/:id", auth, registerForEvent);
 router.get("/registered-events", auth, getRegisteredEvents);
 
 export default router;
